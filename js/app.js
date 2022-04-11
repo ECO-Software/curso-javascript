d75c198945136ea35ec2937b300c649b30036ce6
Andres Sevillano <andres.sevillano.net@gmail.com>
2022-04-11 01:33:38 -0500

    Inicio de pagina para curso JS

---
+++ js/app.js
@@ -0,0 +1,40 @@
+console.log("app.js -> Aprendiendo Variables y comentarios");
+/******************************************  
+*              Variables                  *
+*******************************************/
+let nombre = "Juan"; // Dato de tipo string
+let apellido = "Perez"; // Dato de tipo string
+let edad = 30; // Dato de tipo number
+let fechaNacimiento = new Date(); // Dato de tipo Date
+console.log(nombre, apellido, edad, fechaNacimiento);
+/******************************************
+ *             Constantes                 *
+ ******************************************/
+const PI = 3.1416; // Dato de tipo number
+const DIAS_SEMANA = 7; // Dato de tipo number
+const NOMBRE_APLICACION = "My APP"; // Dato de tipo string
+console.log(PI, DIAS_SEMANA, NOMBRE_APLICACION);
+/******************************************
+*       Tipos de datos primitivos         *
+*******************************************/
+// Boolean
+let estaEstudiando = true; // Dato de tipo boolean
+// Number
+let anioNacimiento = 1990; // Dato de tipo number
+// String
+let nombreCompleto = `${nombre} ${apellido}`; // Dato de tipo string
+// Date
+let hoy = new Date(); // Dato de tipo Date
+// Array
+let cursos = ["TypeScript", "JavaScript", "Angular"]; // Dato de tipo Array
+// Object
+let usuario = {estaEstudiando: true, anioNacimiento: 1990}; // Dato de tipo Object
+console.log(estaEstudiando, anioNacimiento, nombreCompleto, hoy, cursos, usuario);
+console.log(typeof nombre);
+/******************************************
+*               Comentarios               *
+*******************************************/
+// Comentario de una linea
+/*
+* Comentario de varias lineas
+*/
---
+++ js/codigo.js
@@ -0,0 +1,3 @@
+console.log("codigo.js -> Primer JS code");
+console.log("Hola mundo");
+console.log(4 + 4);
