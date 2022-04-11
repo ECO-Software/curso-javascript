f95bd81f5f60f041689b436eea8f1491318e42bc
Andres Sevillano <andres.sevillano.net@gmail.com>
2022-04-11 15:42:02 -0500

    Updated: Generated views and content of strings

---
+++ js/prog004.js
@@ -0,0 +1,21 @@
+let titulo;
+titulo = "Strings";
+console.log(titulo);
+/**
+ * Strings
+ */
+let nombre; // variable para almacenar el nombre
+let apellido; // variable para almacenar el apellido
+let nombre_completo; // variable para almacenar el nombre completo
+let espacio; // variable para almacenar el espacio
+nombre = "Andres"; // asignacion de valor
+apellido = "Sevillano"; // asignacion de valor
+espacio = " "; // asignacion de valor
+nombre_completo = nombre + espacio + apellido; // concatenacion
+console.log(nombre_completo);// imprime el resultado
+console.log("Se a registrado el usuario " + nombre_completo);// imprime el resultado
+nombre = "Bob";// asignacion de valor
+apellido = "Marley";// asignacion de valor
+console.log(nombre, espacio, apellido);// imprime el resultado
+console.log('"DOM" es una libreria para crear paginas web');// imprime el resultado
+console.log(`Se a registrado el usuario ${nombre} ${apellido}`);// imprime el resultado
